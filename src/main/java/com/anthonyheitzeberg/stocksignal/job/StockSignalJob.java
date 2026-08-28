1642096863d1549b84eefc595f330e0209bf6270
Anthony Heitzeberg <a.heitzeberg007@gmail.com>
2026-08-27 21:48:14 -0500

    Chain new jobs to get the keyed stream and then create a window around it and finally apply the aggregator to the events

--- src/main/java/com/anthonyheitzeberg/stocksignal/job/StockSignalJob.java
+++ src/main/java/com/anthonyheitzeberg/stocksignal/job/StockSignalJob.java
@@ -1,9 +1,19 @@
 package com.anthonyheitzeberg.stocksignal.job;
 
+import com.anthonyheitzeberg.stocksignal.aggregate.MovingAverageAggregator;
+import com.anthonyheitzeberg.stocksignal.model.PriceSignal;
 import com.anthonyheitzeberg.stocksignal.model.StockTickEvent;
 import com.anthonyheitzeberg.stocksignal.source.StockTickSource;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+import java.time.Duration;
 
 public class StockSignalJob {
 
@@ -12,7 +22,23 @@ public class StockSignalJob {
 
         DataStream<StockTickEvent> ticks = env.addSource(new StockTickSource());
 
-        ticks.print();
+        WatermarkStrategy<StockTickEvent> watermarkStrategy = WatermarkStrategy
+                .<StockTickEvent>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                .withTimestampAssigner((event, timestamp) -> event.getTimestamp());
+
+        DataStream<StockTickEvent> ticksWithWatermarks =
+                ticks.assignTimestampsAndWatermarks(watermarkStrategy);
+
+        KeyedStream<StockTickEvent, String> keyedTicks =
+                ticksWithWatermarks.keyBy(StockTickEvent::getSymbol);
+
+        WindowedStream<StockTickEvent, String, TimeWindow> windowedTicks =
+                keyedTicks.window(SlidingEventTimeWindows.of(Time.seconds(30), Time.seconds(5)));
+
+        DataStream<PriceSignal> movingAverages =
+                windowedTicks.aggregate(new MovingAverageAggregator());
+
+        movingAverages.print();
 
         env.execute("Stock Signal Engine");
     }
